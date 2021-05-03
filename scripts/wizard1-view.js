058626980b4c0b9ff426c935246f98f9c7a1c056
Akshit Singla <akshitsingla@AK-Pro.local>
2021-05-02 23:37:58 -0400

    Minor visual changes

--- scripts/wizard1-view.js
+++ scripts/wizard1-view.js
@@ -1,8 +1,8 @@
 var wizard1View = {
     generate: function() {
-
+        // SETUP WELCOME MESSAGE
+        view.setWizardMessage("w1.1");
     },
     _private: {
-        
     }
 };
--- scripts/wizard2-view.js
+++ scripts/wizard2-view.js
@@ -1,5 +1,7 @@
 var wizard2View = {
     generate: function() {
+        // SETUP WELCOME MESSAGE
+        view.setWizardMessage("w2");
 
     },
     _private: {
